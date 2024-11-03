659e5a73dc33a857d894d03240e923992fe0c060
Berk <139360217+Beko2023@users.noreply.github.com>
2024-11-03 16:32:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ document.getElementById("get-activity").addEventListener("click", function () {
   fetch("https://apis.scrimba.com/bored/api/activity")
     .then((response) => response.json())
     .then((data) => {
-      document.getElementById("activity").textContent = `${data.activity}!`;
       confetti();
       document.getElementById("container").classList.add("fun-container");
       document.getElementById("header").textContent = `🎉🤖 FunBot! 🤖🎉`;
       document.getElementById("header").classList.add("fun-header");
-      document.getElementById("get-activity").classList.add("fun-btn");
+      document.getElementById("activity").textContent = `${data.activity}!`;
       document.getElementById("activity").classList.add("fun-activity");
+      document.getElementById("get-activity").classList.add("fun-btn");
+      document.getElementById("get-activity").disabled = true;
       setTimeout(function () {
+        document.getElementById("get-activity").disabled = true;
         document.getElementById("header").textContent = `💤🤖 BoredBot 🤖💤`;
         document.getElementById("container").classList.remove("fun-container");
         document.getElementById("get-activity").classList.remove("fun-btn");
         document.getElementById("activity").classList.remove("fun-activity");
         document.getElementById("header").classList.remove("fun-header");
         document.getElementById("activity").textContent = "hmm...what else?";
+        document.getElementById("get-activity").disabled = false;
       }, 4000);
     });
 });
